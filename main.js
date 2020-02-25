5d936d3e08413caafb29d6b30887ae0d89eeb10d
bolajikoyi <56487032+bolajikoyi@users.noreply.github.com>
2020-02-25 23:58:12 +0100

    The responsiveness of the page is completed

---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu-btn').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'))
+// we listen for a click event when class="menu-btn" is clicked, the class="main-menu show" is toggled
